0a7b8d139d428dd7c945b63d8a785131ecd221fb
sareto <duncan.moiyo@student.moringaschool.com>
2021-05-02 15:17:55 +0300

    add object array with shade and cow infomation

---
+++ script.js
@@ -0,0 +1,27 @@
+const production = [{
+        shadeName: 'shade A',
+        dailyProductionPerCow: 10,
+        numberOfCows: 51,
+        breed: 'jersey',
+    },
+    {
+        shadeName: 'shade B',
+        dailyProductionPerCow: 15.4,
+        numberOfCows: 20,
+        breed: 'Guernsey',
+    },
+    {
+        shadeName: 'shade C',
+        dailyProductionPerCow: 16.2,
+        numberOfCows: 30,
+        breed: 'Friesian ',
+
+
+    },
+    {
+        shadeName: 'shade D',
+        dailyProductionPerCow: 14.3,
+        numberOfCows: 40,
+        breed: 'Simmental ',
+    },
+];
